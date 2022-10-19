b5ba2a661d099c8c9e3d8235e187bad7b29aba0f
lhiser <lindshiser@gmail.com>
2022-10-19 17:19:33 -0500

    Upload bar chart exercise

---
+++ week_03/homework.js
@@ -0,0 +1,60 @@
+/* Bar chart for library visits */
+
+d3.csv("library_visits_jan22.csv").then(data => {
+
+    for (let d of data) {
+        d.cases = +d.cases; // force a number
+    };
+
+    // sort alphabetically
+    data.sort((a, b) => d3.ascending(a.branch, b.branch));
+
+    const height = 600,
+        width = 800,
+        margin = ({ top: 25, right: 30, bottom: 35, left: 50 });
+
+    // resize in browser
+    let svg = d3.select("#chart")
+    .append("svg")
+    .attr("viewBox", [0,0, width, height]);
+
+    // define x-ranges
+    let x = d3.scaleBand()
+    .domain(data.map(d => d.branch))
+    .range([margin.left, width - margin.right])
+    .padding(0.1);
+
+    // define y-ranges
+    let y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.num)]).nice() // nice() rounds the top num
+    .range([height - margin.bottom, margin.top]);
+
+    // group svg shapes together
+    svg.append("g")
+    .attr("transform", `translate(${margin.left - 5},0)`)
+    .call(d3.axisLeft(y));
+
+    // create bar groups
+    let bar = svg.selectAll(".bar")
+    .append("g")
+    .data(data)
+    .join("g")
+    .attr("class", "bar");
+
+    // add rectangles as bars
+    bar.append("rect")
+    .attr("fill", "steelblue")
+    .attr("x", d => x(d.branch)) // x position attribute
+    .attr("width", x.bandwidth()) // this width is the width attr on the element
+    .attr("y", d => y(d.num)) // y position element
+    .attr("height", d => y(0) - y(d.num)); // this height is the height attr on element
+
+    // add labels to bars
+    bar.append('text')
+    .text(d => d.num)
+    .attr('x', d => x(d.branch) + (x.bandwidth()/2))
+    .attr('y', d => y(d.num) + 15)
+    .attr('text-anchor', 'middle')
+    .style('fill', 'white');
+
+});
