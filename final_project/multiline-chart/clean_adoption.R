e2b8582787d453eb266a26bbd97872431c4b1959
lhiser <lindshiser@gmail.com>
2022-11-19 14:27:05 -0600

    Updated code for multiline (adoption)

--- final_project/multiline-chart/clean_adoption.R
+++ final_project/multiline-chart/clean_adoption.R
@@ -1,35 +1,23 @@
 library(tidyverse)
 options(scipen = 9999)
 
-setwd("/Users/lindsayhiser/Documents/Harris/4_FA22/Data Visualization/CAPP30239_FA22/final_project")
-
 path <- "/Users/lindsayhiser/Documents/Harris/4_FA22/Data Visualization/CAPP30239_FA22/final_project"
-
 raw_adoption <- read_csv(file.path(path, "/data/technology-adoption-by-households-in-the-united-states.csv"))
 raw_adoption <- head(raw_adoption, -2) # remove last two rows
 
 # codebook
 technologies <- tibble(
-  name_og = c('Cable TV', 'Cellular phone', 'Colour TV', 'Computer', 'Ebook reader', 
-              'Households with only mobile phones (no landlines)', 'Internet', 
-              'Landline', 'Podcasting', 'Radio', 'Social media usage', 'Tablet', 
-              'Television', 'Videocasette recorder'),
-  name_new = c('Cable TV', 'Cell phone', 'Color TV', 'Computer', 'Ebook reader', 
-               'Cell phone only (no landline)', 'Internet', 'Landline', 'Podcasting', 
-               'Radio', 'Social media', 'Tablet', 'TV', 'Videocasette recorder'),
-)
+  name = c('Cellular phone', 'Colour TV', 'Computer', 'Landline', 'Radio', 'Tablet'),
+  intro = c(1973, 1954, 1977, 1876, 1923, 2010))
 
 # clean
 df <- raw_adoption %>%
-  filter(item %in% technologies$name_og) %>%
+  filter(item %in% technologies$name) %>%
   rename('technology' = item) %>%
-  mutate(share = as.integer(share)) %>% # remove decimals
+  mutate(share = as.integer(share), # remove decimals
+         technology = recode(technology, 'Cellular phone' = 'Cell phone', # rename values
+                                         'Colour TV' = 'Color TV')) %>% 
   select(technology, year, share)
-
-
-# replace column names
-
-
 
 # write csv
 write_csv(df, 'multiline-chart/adoption.csv')
---
+++ final_project/multiline-use/clean_time_use.R
@@ -0,0 +1,68 @@
+# https://www.kaggle.com/datasets/bls/american-time-use-survey?resource=download
+
+library(tidyverse)
+options(scipen = 9999)
+
+path <- "/Users/lindsayhiser/Documents/Harris/4_FA22/Data Visualization/CAPP30239_FA22/final_project"
+
+raw_atus_act <- read_csv(file.path(path, "/data/atusact.csv"))
+
+activities <- tibble(
+  name = c("TV and movies", "TV (religious)", "Listening to radio", 
+            "Listening to music", "Playing games", "Computer use (for leisure)", 
+            "Telephone with family", "Telephone with friends"),
+  code = c("120303", "120304", "120305", "120306", "120307", "120308", "160101", "160102")
+)
+  
+# clean act
+df <- raw_atus_act %>%
+  select(tucaseid, tuactivity_n, trcodep, tuactdur24) %>%
+  rename("respondent" = tucaseid,
+         "actv_num" = tuactivity_n,
+         "actv_code" = trcodep,
+         "minutes" = tuactdur24)
+
+# filter activities
+df <- df %>%
+  filter(actv_code %in% activities$code)
+
+# combine times for multiple instances of single activity
+df <- df %>%
+  group_by(respondent, actv_code) %>%
+  summarise(minutes = sum(minutes)) %>%
+  pivot_wider(names_from = actv_code, values_from = minutes)
+
+# separate respondent col into years
+df <- df %>%
+  separate(respondent, into = c("year", "respondent"), sep = 4)
+
+df$x <- df$'120303'
+
+df %>%
+  group_by(year) %>%
+  summarise(mean = mean(x, na.rm = TRUE))
+df %>%
+  group_by(year) %>%
+  summarise('120303' = mean([2], na.rm = TRUE),
+            '120304' = mean([3], na.rm = TRUE),
+            '120305' = mean('120305', na.rm = TRUE),
+            '120306' = mean('120306', na.rm = TRUE),
+            '120307' = mean('120307', na.rm = TRUE),
+            '120308' = mean('120308', na.rm = TRUE),
+            '160101' = mean('160101', na.rm = TRUE),
+            '160102' = mean('160102', na.rm = TRUE)) %>%
+  pivot_longer(c('120303', '120304', '120305', '120306', '120307', '120308', '160101', '160102'), 
+               names_to = "activity", values_to = "avg_min")
+?mean()
+
+
+# replace column names
+replace_colnames <- activities$code %in% names(df)
+df <- df %>%
+  select("120303", "120304", "120305", "120306", "120307", "120308", "160101", "160102") %>%
+  top_n(8) %>%
+  rename_with(~activities$name[replace_colnames], activities$code[replace_colnames])
+# source: https://stackoverflow.com/questions/73390056/how-do-i-rename-columns-in-tidyverse-with-vectors-of-names
+
+
+
